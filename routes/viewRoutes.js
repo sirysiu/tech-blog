5b3b951746dc71de6b1c635be1516ff31dcacbe9
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-02-05 15:09:38 -0500

    updated user routes and login

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,33 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
 
 router.get('/', async (req, res) => {
-  res.render('index');
+  try {
+    res.render('index');
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', withAuth, (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
   res.render('dashboard');
 });
+
 
 module.exports = router;
